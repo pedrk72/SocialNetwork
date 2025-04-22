b37c8f51ef697be9d3c3eba8bdf134fa746aa7cb
pedrk72 <79995742+pedrk72@users.noreply.github.com>
2025-04-22 18:21:20 +0100

    CRUD operations added to UserResource class

--- quarkus-social/src/main/java/pedrk72/quarkusSocial/rest/UserResource.java
+++ quarkus-social/src/main/java/pedrk72/quarkusSocial/rest/UserResource.java
@@ -1,7 +1,11 @@
 package pedrk72.quarkusSocial.rest;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import pedrk72.quarkusSocial.domain.model.User;
 import pedrk72.quarkusSocial.rest.dto.CreateUserRequest;
 
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,12 +16,47 @@ public class UserResource  {
     @POST
     @Consumes(MediaType.APPLICATION_JSON) //Which type of data the method will receive
     @Produces(MediaType.APPLICATION_JSON) //Which type of data the method will return
+    @Transactional
     public Response createUser(CreateUserRequest userRequest){
-        return Response.ok(userRequest).build();
+        User user = new User();
+        user.setName(userRequest.getName());
+        user.setAge(userRequest.getAge());
+        user.persist();
+        return Response.ok(user).build();
     }
 
     @GET
     public Response listAllUsers(){
-        return Response.ok().build();
+        PanacheQuery<PanacheEntityBase> query = User.findAll();
+        return Response.ok(query.list()).build();
+    }
+
+    @DELETE
+    @Transactional
+    @Path("{id}")
+    public Response deleteUser(@PathParam("id") Long id){
+        User byId = User.findById(id);
+
+        if (byId != null){
+            byId.delete();
+            return Response.ok().build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @PUT
+    @Transactional
+    @Path("{id}")
+    public Response updateUser(@PathParam("id") Long id, CreateUserRequest userData){
+        User byId = User.findById(id);
+
+        if (byId != null){
+            byId.setName(userData.getName());
+            byId.setAge(userData.getAge());
+            return Response.ok(byId).build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 }
