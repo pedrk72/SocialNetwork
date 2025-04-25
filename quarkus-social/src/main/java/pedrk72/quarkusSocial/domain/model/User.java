5036a2c876fb4e0fb84fe383770e7b6ad0946b31
pedrk72 <79995742+pedrk72@users.noreply.github.com>
2025-04-25 14:46:06 +0100

    Lombok dependency added to the project

--- quarkus-social/src/main/java/pedrk72/quarkusSocial/domain/model/User.java
+++ quarkus-social/src/main/java/pedrk72/quarkusSocial/domain/model/User.java
@@ -2,12 +2,14 @@ package pedrk72.quarkusSocial.domain.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity //To indicate that is an entity of the database.
 @Table(name = "users") // To indicate the respective table name on DB.
+@Data
 public class User {
 
     @Id
@@ -20,30 +22,6 @@ public class User {
     @Column(name = "age")
     private Integer age;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
     //Method equals is important to the comparison between objects.
     @Override
     public boolean equals(Object o) {
@@ -53,7 +31,7 @@ public class User {
         return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(age, user.age);
     }
 
-    //Method equals is important to the comparison between objects.
+    //Method hashCode is important to the comparison between objects.
     @Override
     public int hashCode() {
         return Objects.hash(id, name, age);
--- quarkus-social/src/main/java/pedrk72/quarkusSocial/rest/dto/CreateUserRequest.java
+++ quarkus-social/src/main/java/pedrk72/quarkusSocial/rest/dto/CreateUserRequest.java
@@ -1,8 +1,13 @@
 package pedrk72.quarkusSocial.rest.dto;
 
+import lombok.Data;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+//@Getter
+//@Setter
+@Data //Annotations that set for us the getters, setters and constructors methods.
 public class CreateUserRequest {
 
     @NotBlank(message = "Name is required") //To verify if the String is not empty or null
@@ -11,19 +16,20 @@ public class CreateUserRequest {
     @NotNull(message = "Age is required") //To verify if the String is not null
     private Integer age;
 
-    public String getName() {
-        return name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
+    // With lombok we don't need anymore to instantiate the gets and setters methods
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public Integer getAge() {
+//        return age;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setAge(Integer age) {
+//        this.age = age;
+//    }
 }
